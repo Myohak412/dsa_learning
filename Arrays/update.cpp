5a2aac96de1bd3d873688f4fe51a31ae556e2d19
Mohak W <MohakW@gmail.com>
2025-07-21 11:47:32 +0530

    Small changes done

--- Arrays/update.cpp
+++ Arrays/update.cpp
@@ -4,7 +4,7 @@ int main(){
     int arr[5]= {2,5,7,9,1};
    
      arr[4] = 10;
-      cout << "Element updated in array 4th is: " << arr[4];
+      cout << "Element updated in array 4th is: " << arr[4] << endl;
       //cout << arr[5];
     return 0;
 }
