4dfc936bec78070abb198e673d508887a74e3b38
Mohak W <mohakwaghmare2002@gmail.com>
2025-07-28 12:00:00 +0530

    link lists user input for two nodes done

---
+++ LinkLists/userInput2N.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+};
+int main(){
+
+int val1, val2;
+cout << "Enter the value for first node:  ";
+cin >> val1;
+
+cout << "Enter the value for first node:  ";
+cin >> val2;
+
+//creating the first node
+Node* first = new Node();
+first -> data = val1;
+first -> next = NULL;
+//creating for second node
+Node* second = new Node();
+second -> data = val2;
+second -> next = NULL;
+//connection
+first -> next = second;
+
+Node* temp = first;
+while(temp != NULL ){
+    cout << temp -> data << " -> ";
+    temp = temp -> next;
+}
+cout << " Null " << endl;
+return 0;
+}
