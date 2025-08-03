aaef34e88218b04e7638e9100b90dfc3902b06c8
Mohak W <mohakwaghmare2002@gmail.com>
2025-08-03 12:00:00 +0530

    iota method in vectors done

---
+++ Vectors/iota.cpp
@@ -0,0 +1,14 @@
+//keep learning babes;-) and I m about to done with be these topics
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v(5);
+    iota(v.begin(),v.end(), 15);
+    for(auto i : v)
+    cout<<i<<"  ";
+    return 0;
+
+}
