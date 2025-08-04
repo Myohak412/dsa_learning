59c69cada350d4024dc9a3fe9cf00eea355fde85
Mohak W <mohakwaghmare2002@gmail.com>
2025-08-04 12:00:00 +0530

    practise

--- Vectors/iota.cpp
+++ Vectors/iota.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(){
     vector<int> v(5);
-    iota(v.begin(),v.end(), 15);
+    iota(v.begin(),v.end(), 15);// takes consecutive  values
     for(auto i : v)
     cout<<i<<"  ";
     return 0;
