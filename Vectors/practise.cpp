2d7381c5b6e6a6893145f7bc414b290ee4960a88
Mohak W <mohakwaghmare2002@gmail.com>
2025-08-05 18:17:45 +0530

    Vector revision done

---
+++ Vectors/practise.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+int main(){
+    vector<int> v1 = {1,2,3,4,5};
+    for(auto i:v1){
+        cout << i ;
+    }
+    return 0;
+}
+    */
+
+int main(){
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    for(auto i:v)
+        cout << i << "  " ;
+        
+        return 0;
+
+}
